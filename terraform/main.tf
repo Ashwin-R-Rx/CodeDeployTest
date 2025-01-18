690d964170a4444e931f79a56f309f0c98b40616
Ashwin-R-Code-Rx <ashwinstar1950@gmail.com>
2025-01-18 22:28:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,13 @@
 provider "aws" {
-  region = "us-west-2"  # Change this to your desired AWS region
+  region = "us-east-1"  # Change to your desired AWS region
 }
 
-# Attempt to find the SQS queue (this will fail if it doesn't exist)
-data "aws_sqs_queue" "existing_queue" {
-  name = "my-queue-name"  # Replace with your desired queue name
-}
+# Create the SQS queue, it will not duplicate if it already exists
+resource "aws_sqs_queue" "queue" {
+  name = "TerraformSQSQueue"  # Replace with your desired queue name
 
-# Create the SQS queue only if it doesn't exist (use count to check)
-resource "aws_sqs_queue" "new_queue" {
-  count = length(try(data.aws_sqs_queue.existing_queue.id, [])) == 0 ? 1 : 0
-
-  name = "my-queue-name"  # Replace with your desired queue name
+  # Ensure the queue will be created if it doesn't exist
+  lifecycle {
+    prevent_destroy = true  # Prevents accidental destruction of the queue
+  }
 }

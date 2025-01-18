b787cf8ceef88c19314e696c0eb3fd04b74357f4
Ashwin-R-Code-Rx <ashwinstar1950@gmail.com>
2025-01-18 21:48:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,20 @@
 provider "aws" {
-  region = "us-east-1" # Replace with your preferred AWS region
+  region = "us-east-1"  # Specify your AWS region
 }
 
-variable "queue_name" {
-  default = "GitHubActionsQueue"
+resource "aws_sqs_queue" "my_queue" {
+  name = "TerraformSQSQueue"
 }
 
-resource "aws_sqs_queue" "example" {
-  name                        = var.queue_name
-  delay_seconds               = 0
-  max_message_size            = 262144
-  message_retention_seconds   = 10000
-  visibility_timeout_seconds  = 10
-
-  tags = {
-    Environment = "Dev"
-    Project     = "GitHubActionsExample"
-  }
+resource "aws_sqs_queue_message" "my_message" {
+  queue_url = aws_sqs_queue.my_queue.url
+  message_body = "Terraform SQSQueue Message!"
 }
 
 output "queue_url" {
-  value = aws_sqs_queue.example.id
+  value = aws_sqs_queue.my_queue.url
+}
+
+output "message_id" {
+  value = aws_sqs_queue_message.my_message.message_id
 }

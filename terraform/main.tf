8adfd5198df9cbc9cdb5bb50f7d22bd92450c362
Ashwin-R-Code-Rx <ashwinstar1950@gmail.com>
2025-01-18 21:57:32 +0530

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,15 +6,6 @@ resource "aws_sqs_queue" "my_queue" {
   name = "TerraformSQSQueue"
 }
 
-resource "aws_sqs_queue_message" "my_message" {
-  queue_url = aws_sqs_queue.my_queue.url
-  message_body = "Terraform SQSQueue Message!"
-}
-
 output "queue_url" {
   value = aws_sqs_queue.my_queue.url
-}
-
-output "message_id" {
-  value = aws_sqs_queue_message.my_message.message_id
 }

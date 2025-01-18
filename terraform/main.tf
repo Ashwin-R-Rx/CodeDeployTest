88077b71fe2e4379016948c212ba58c9dc049d0d
Ashwin-R-Code-Rx <ashwinstar1950@gmail.com>
2025-01-18 22:16:58 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,15 @@
 provider "aws" {
-  region = "us-east-1"  # Specify your AWS region
+  region = "us-east-1"  # Change this to your desired AWS region
 }
 
-resource "aws_sqs_queue" "my_queue" {
-  name = "TerraformSQSQueue"
+# Check if the SQS queue already exists
+data "aws_sqs_queue" "existing_queue" {
+  name = "GitHubActionsQueue"  # Replace with the desired queue name
 }
 
-output "queue_url" {
-  value = aws_sqs_queue.my_queue.url
+# If the queue does not exist, create a new one
+resource "aws_sqs_queue" "new_queue" {
+  count = length(data.aws_sqs_queue.existing_queue.id) == 0 ? 1 : 0
+
+  name = "GitHubActionsQueue"  # Replace with your queue name
 }

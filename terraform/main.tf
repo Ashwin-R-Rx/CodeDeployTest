5cd725cc4eab2d40c694e1ebb041ae396548c4aa
Ashwin-R-Code-Rx <ashwinstar1950@gmail.com>
2025-01-18 22:19:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 # Check if the SQS queue already exists
 data "aws_sqs_queue" "existing_queue" {
-  name = "GitHubActionsQueue"  # Replace with the desired queue name
+  name = "TerraformSQSQueue"  # Replace with the desired queue name
 }
 
 # If the queue does not exist, create a new one
 resource "aws_sqs_queue" "new_queue" {
   count = length(data.aws_sqs_queue.existing_queue.id) == 0 ? 1 : 0
 
-  name = "GitHubActionsQueue"  # Replace with your queue name
+  name = "TerraformSQSQueue"  # Replace with your queue name
 }
